a0ced39332156e82b05c356dcc4cd24d7221208c
shahzaib <shahzaibxzabi@gmail.com>
2025-03-26 23:49:20 +0500

    task2 done

---
+++ Lab 10/Task 2/task2proxy.java
@@ -0,0 +1,54 @@
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        System.out.println("Loading image from disk: " + fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying image: " + fileName);
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
+
+public class task2proxy {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+        System.out.println("\nFirst display call:");
+        image1.display();
+
+        System.out.println("\nSecond display call:");
+        image1.display();
+
+        System.out.println("\nDisplaying another image:");
+        image2.display();
+    }
+}
